9e36a4afdaa84206a22a99fcb19a48c34f05aa42
mrtrvl <mrt@mrt.ee>
2021-03-20 15:03:40 +0200

    Change database connection to pool

--- db.js
+++ db.js
@@ -1,18 +1,25 @@
+// Get the client
 const mysql = require('mysql2');
 const util = require('util');
 const { db } = require('./config');
 
-const connection = mysql.createConnection({
+// Create the connection to database
+const pool = mysql.createPool({
   host: db.host,
   user: db.user,
   password: db.password,
-  port: db.port,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-connection.query(`CREATE SCHEMA IF NOT EXISTS ${db.database}`, () => {
-  connection.query(`USE ${db.database}`, () => {});
-});
+pool.query(
+  `CREATE SCHEMA IF NOT EXISTS ${db.database}`, () => {
+    pool.query(`USE ${db.database}`, () => {
+    });
+  },
+);
 
-connection.query = util.promisify(connection.query);
+pool.query = util.promisify(pool.query);
 
-module.exports = connection;
+module.exports = pool;
