af86f5f1e798d616109f180dd746c2781560a284
mrtrvl <mrt@mrt.ee>
2021-03-20 15:31:32 +0200

    Add database connection to comments

--- api/controllers/commentsController.js
+++ api/controllers/commentsController.js
@@ -9,14 +9,14 @@ const commentsController = {};
  * Optional values: none
  * Success: status 200 - OK and list of comments
  */
-commentsController.getComments = (req, res) => {
+commentsController.getComments = async (req, res) => {
   let comments;
   const createdById = req.userId;
   const role = req.userRole;
   if (role === 'Admin') {
-    comments = commentsService.getAllComments();
+    comments = await commentsService.getAllComments();
   } else {
-    comments = commentsService.getUserComments(createdById);
+    comments = await commentsService.getUserComments(createdById);
   }
   res.status(200).json({
     comments,
@@ -31,11 +31,11 @@ commentsController.getComments = (req, res) => {
  * Success: status 200 - OK and comment with specified id
  * Error: status 400 - Bad Request and error message
  */
-commentsController.getCommentById = (req, res) => {
+commentsController.getCommentById = async (req, res) => {
   const id = parseInt(req.params.id, 10);
   const createdById = req.userId;
   const isAdmin = req.userRole === 'Admin';
-  const comment = commentsService.getCommentById(id);
+  const comment = await commentsService.getCommentById(id);
   // Check if comment exists and user is creator of this comment or admin
   if (!(comment.createdById === createdById || isAdmin) || !comment) {
     return res.status(400).json({
@@ -55,7 +55,7 @@ commentsController.getCommentById = (req, res) => {
  * Success: status 201 - Created and id of created comment
  * Error: status 400 - Bad Request and error message
  */
-commentsController.createComment = (req, res) => {
+commentsController.createComment = async (req, res) => {
   const { excuseId, content } = req.body;
   const createdById = req.userId;
   if (!excuseId || !content) {
@@ -68,7 +68,7 @@ commentsController.createComment = (req, res) => {
     createdById,
     content,
   };
-  const id = commentsService.createComment(comment);
+  const id = await commentsService.createComment(comment);
   return res.status(201).json({
     id,
   });
@@ -82,12 +82,12 @@ commentsController.createComment = (req, res) => {
  * Success: status 204 - No Content
  * Error: status 400 - Bad Request and error message
  */
-commentsController.deleteComment = (req, res) => {
+commentsController.deleteComment = async (req, res) => {
   const id = parseInt(req.params.id, 10);
   const createdById = req.userId;
   const isAdmin = req.userRole === 'Admin';
   // Check if comment exists
-  const comment = commentsService.getCommentById(id);
+  const comment = await commentsService.getCommentById(id);
   if (!comment) {
     return res.status(400).json({
       error: `No comment found with id: ${id}`,
@@ -99,7 +99,7 @@ commentsController.deleteComment = (req, res) => {
       error: 'You have no rights to delete this comment',
     });
   }
-  const success = commentsService.deleteComment(id);
+  const success = await commentsService.deleteComment(id);
   if (!success) {
     res.status(500).json({
       error: 'Something went wrong while deleting comment.',
--- api/services/commentsService.js
+++ api/services/commentsService.js
@@ -1,45 +1,71 @@
-const database = require('../../database');
+const db = require('../../db');
 
 const commentsService = {};
 
 // Returns all comments
-commentsService.getAllComments = () => {
-  const { comments } = database;
+commentsService.getAllComments = async () => {
+  const comments = await db.query(
+    `SELECT
+      C.id, C.content, U.id AS createdById, U.firstName, U.lastName, U.email, E.id AS excuseId, E.description
+    FROM
+      comments C
+    INNER JOIN
+      users U ON C.createdById = U.id
+    INNER JOIN
+      excuses E ON C.excuseId = E.id
+    WHERE
+      C.deleted = 0`,
+  );
   return comments;
 };
 
 // Returns users comments
-commentsService.getUserComments = (userId) => {
-  const comments = database.comments.filter((element) => element.createdById === userId);
+commentsService.getUserComments = async (userId) => {
+  const comments = await db.query(
+    `SELECT
+      C.id, C.content, E.id AS excuseId, E.description
+    FROM
+      comments C
+    INNER JOIN
+      users U ON C.createdById = U.id
+    INNER JOIN
+      excuses E ON C.excuseId = E.id
+    WHERE
+      U.id = ? AND C.deleted = 0`, [userId],
+  );
   return comments;
 };
 
 // Find comment by id. Returns comment if found or false.
-commentsService.getCommentById = (id) => {
-  const comment = database.comments.find((element) => element.id === id);
-  if (comment) {
-    return comment;
+commentsService.getCommentById = async (id) => {
+  const comments = await db.query(
+    `SELECT
+      C.id, C.content, U.id AS createdById, U.firstName, U.lastName, U.email, E.id AS excuseId, E.description
+    FROM
+      comments C
+    INNER JOIN
+      users U ON C.createdById = U.id
+    INNER JOIN
+      excuses E ON C.excuseId = E.id
+    WHERE
+      C.id = ? AND C.deleted = 0`, [id],
+  );
+  if (comments[0]) {
+    return comments[0];
   }
   return false;
 };
 
 // Creates new comment
-commentsService.createComment = (newComment) => {
-  const id = database.comments.length + 1;
-  const comment = {
-    id,
-    ...newComment,
-  };
-  database.comments.push(comment);
+commentsService.createComment = async (comment) => {
+  const result = await db.query('INSERT INTO comments SET ?', [comment]);
+  const id = result.insertId;
   return id;
 };
 
 // Deletes comment
-commentsService.deleteComment = (id) => {
-  // Find comment index
-  const index = database.comments.findIndex((element) => element.id === id);
-  // Remove comment from 'database'
-  database.comments.splice(index, 1);
+commentsService.deleteComment = async (id) => {
+  await db.query('UPDATE comments SET deleted = 1 WHERE id = ?', [id]);
   return true;
 };
 
