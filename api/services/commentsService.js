0d971ae9fd4f6ed087b66f7039bc9b2a07c3b778
mrtrvl <mrt@mrt.ee>
2021-03-20 16:58:32 +0200

    Format comments output to more readable format

--- api/services/commentsService.js
+++ api/services/commentsService.js
@@ -1,12 +1,31 @@
 const db = require('../../db');
 
+const formatOutput = async (results) => {
+  const formatted = await results.map((comment) => ({
+    id: comment.id,
+    content: comment.content,
+    dateCreated: comment.dateCreated,
+    createdBy: {
+      id: comment.createdById,
+      firstName: comment.firstName,
+      lastName: comment.lastName,
+      email: comment.email,
+    },
+    excuse: {
+      id: comment.excuseId,
+      description: comment.excuseDescription,
+    },
+  }));
+  return formatted;
+};
+
 const commentsService = {};
 
 // Returns all comments
 commentsService.getAllComments = async () => {
   const comments = await db.query(
     `SELECT
-      C.id, C.content, U.id AS createdById, U.firstName, U.lastName, U.email, E.id AS excuseId, E.description
+      C.id, C.content, C.dateCreated, U.id AS createdById, U.firstName, U.lastName, U.email, E.id AS excuseId, E.description AS excuseDescription
     FROM
       comments C
     INNER JOIN
@@ -16,14 +35,16 @@ commentsService.getAllComments = async () => {
     WHERE
       C.deleted = 0`,
   );
-  return comments;
+
+  const formattedOutput = await formatOutput(comments);
+  return formattedOutput;
 };
 
 // Returns users comments
 commentsService.getUserComments = async (userId) => {
   const comments = await db.query(
     `SELECT
-      C.id, C.content, E.id AS excuseId, E.description
+      C.id, C.content, C.dateCreated, E.id AS excuseId, E.description AS excuseDescription
     FROM
       comments C
     INNER JOIN
@@ -33,14 +54,15 @@ commentsService.getUserComments = async (userId) => {
     WHERE
       U.id = ? AND C.deleted = 0`, [userId],
   );
-  return comments;
+  const formattedOutput = await formatOutput(comments);
+  return formattedOutput;
 };
 
 // Find comment by id. Returns comment if found or false.
 commentsService.getCommentById = async (id) => {
   const comments = await db.query(
     `SELECT
-      C.id, C.content, U.id AS createdById, U.firstName, U.lastName, U.email, E.id AS excuseId, E.description
+      C.id, C.content, C.dateCreated, U.id AS createdById, U.firstName, U.lastName, U.email, E.id AS excuseId, E.description AS excuseDescription
     FROM
       comments C
     INNER JOIN
@@ -50,8 +72,9 @@ commentsService.getCommentById = async (id) => {
     WHERE
       C.id = ? AND C.deleted = 0`, [id],
   );
-  if (comments[0]) {
-    return comments[0];
+  const formattedOutput = await formatOutput(comments);
+  if (formattedOutput[0]) {
+    return formattedOutput[0];
   }
   return false;
 };
